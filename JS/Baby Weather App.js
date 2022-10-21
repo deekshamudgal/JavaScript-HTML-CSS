b9ae9faf1cf944b6406a32b43d3c898878ca866d
Deeksha Mudgal <73310019+deekshamudgal@users.noreply.github.com>
2022-10-21 10:43:44 +0530

    Create Baby Weather App.js

---
+++ JS/Baby Weather App.js
@@ -0,0 +1,11 @@
+function babyWeatherApp(){
+let weather=prompt('How is the weather today?')
+if(weather=='rainy'){
+  console.log('Grab an umbrella 🌈')
+}
+else{
+  console.log('Put on a pair of sunglasses 😎')
+}
+}
+
+babyWeatherApp()
